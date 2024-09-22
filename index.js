6f3e0e57b67f5d37475e2b5be83a00b9b2800578
pkamanthe <patriciamumbua97@gmail.com>
2024-09-22 16:19:30 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+// Employee object
+let employee = {
+    name: "John Doe",
+    streetAddress: "123 Main St"
+};
+
+// Function to non-destructively update employee
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+}
+
+// Function to destructively update employee
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+// Function to non-destructively delete a key from employee
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+// Function to destructively delete a key from employee
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
